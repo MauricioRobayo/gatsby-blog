056a3a5e5c62a5ae8f4ff8cd9ccea82f2cf60971
Mauricio Robayo <rfmajo@gmail.com>
2021-11-13 17:59:50 -0500

    Add about page
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to="/">Home</Link>
+      <p>This is the about page</p>
+    </div>
+  );
+};
+
+export default AboutPage;
