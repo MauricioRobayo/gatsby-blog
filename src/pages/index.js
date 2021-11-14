ad9541f75f4775f74d95916ca948bd6b7ada3aec
Mauricio Robayo <rfmajo@gmail.com>
2021-11-13 20:59:29 -0500

    Add image plugin
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home page">
       <main>
         <p>I'm making this by following Gastby Tutorial.</p>
+        <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="../images/E1oMV3QVgAIr1NT.webp"
+        />
       </main>
     </Layout>
   );
